3bcef59062d7234ba064654eb719757010fa76ed
Narayan soni <narayansoni854@gmail.com>
2023-04-06 12:11:19 +0530

    configured bodyparser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+// Middleware to parse JSON and urlencoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create a route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Server is up and running!");
 });
 
 // Start the server
