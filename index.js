f4243ef4950564c32a22299f251746d6b049e0d4
Narayan soni <narayansoni854@gmail.com>
2023-04-06 21:49:31 +0530

    implement streaming functionality

--- index.js
+++ index.js
@@ -38,7 +38,42 @@ app.post("/upload", (req, res) => {
   });
 });
 
-// Create a route
+// Route to stream the video
+app.get("/stream/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const path = `./uploads/${filename}`;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+});
+
+
+// Create base route
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
 });
